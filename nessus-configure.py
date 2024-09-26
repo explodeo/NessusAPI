accdb53f9f20d0a279821f10654ee94166c711e4
Chris M <21688716+explodeo@users.noreply.github.com>
2024-09-26 19:03:51 -0400

    Initial Commit

---
+++ nessus-configure.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+import code
+from sys import argv
+from nessusapi import NessusAPI
+
+if __name__ == "__main__":
+    if any(arg in ['-h', '--help'] for arg in argv):
+        print(f"USAGE: {argv[0]} /path/to/config.json [-h|--help] [--interactive] [--initialize]")
+        exit(0)
+    if not os.path.exists(argv[1]):
+        raise ValueError("ERROR: Config file does not Exist")
+    nessus = NessusAPI(file=argv[1], initialize=('--initialize' in argv))
+    if '--interactive' in argv:
+        from pprint import pprint
+        printvars = lambda obj: pprint(vars(obj))
+        n = nessus.Nessus
+        code.interact(local=locals())
+    nessus.logout()
